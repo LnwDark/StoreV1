29fee011162d1d6d80bc412d8cfdb2ba53fc8b09
LNWDARK <hnam@nextgensoft.co.th>
2018-12-06 18:01:17 +0700

    first commit

--- index.php
+++ index.php
@@ -128,16 +128,17 @@ require __DIR__ . '/vendor/autoload.php';
 use Mike42\Escpos\Printer;
 use Mike42\Escpos\EscposImage;
 use Mike42\Escpos\PrintConnectors\FilePrintConnector;
+
 $connector = new \Mike42\Escpos\PrintConnectors\WindowsPrintConnector("POS581");
 //$connector = new FilePrintConnector("POS58");
 $printer = new Printer($connector);
 /* Initialize */
 //$id =$argv[1];
-$id = !empty($_GET['id'])?$_GET['id']:'';
-$Locations = !empty($_GET['lct'])?$_GET['lct']:'';
-$qty = !empty($_GET['amount'])?$_GET['amount']:'';
-$date =!empty($_GET['created_at'])?$_GET['created_at']:'';
-$part_name = !empty($_GET['part_name'])?$_GET['part_name']:'';
+$id = !empty($_GET['id']) ? $_GET['id'] : '';
+$Locations = !empty($_GET['lct']) ? $_GET['lct'] : '';
+$qty = !empty($_GET['amount']) ? $_GET['amount'] : '';
+$date = !empty($_GET['created_at']) ? $_GET['created_at'] : '';
+$part_name = !empty($_GET['part_name']) ? $_GET['part_name'] : '';
 //$Locations = 'P-test';
 //$qty = '2000';
 //$id ='1539688124';
@@ -146,14 +147,12 @@ $position = Printer::BARCODE_TEXT_NONE;
 // $printer->text($id);
 
 if (!empty($id)) {
-//    $printer->feed(6);
     $printer->setJustification(Printer::JUSTIFY_CENTER);
-    $printer->text("N:". $part_name .' '. 'Q:'.$qty. "\n");
+    $printer->text("N:" . $part_name . ' ' . 'Q:' . $qty . "\n");
     $printer->setBarcodeHeight(50);
-//    $printer->setBarcodeWidth(200);
     $printer->setBarcodeTextPosition($position);
     $printer->barcode($id, $type);
-    $printer->text("L:". $Locations ." D:".$date);
+    $printer->text("L:" . $Locations . " D:" . $date);
     $printer->feed();
     $printer->feed(1);
     $printer->feed();
@@ -166,7 +165,8 @@ if (!empty($id)) {
 ////    $printer -> setJustification();
 //    $printer -> feed();
 //    $printer -> qrCode($testStr, Printer::QR_ECLEVEL_L, 1);
-    $printer->close();
+
 }
+$printer->close();
 
 ?>
